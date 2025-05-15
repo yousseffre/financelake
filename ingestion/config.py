2d0c1b588933ea8d637076e1f2f0a59b4e89a49f
MOUSAAB-ABOUJAAFAR <123aboujaafar@gmail.com>
2025-05-15 19:25:22 +0100

    financeLake data ingestion process with apache kafka & apache nifi with ui dashboard template

---
+++ ingestion/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOPIC = os.getenv("TOPIC")
+HDFS_BASE_DIR = os.getenv("HDFS_BASE_DIR")
+LOCAL_LOG_DIR = os.getenv("LOCAL_LOG_DIR")
+API_KEY = os.getenv("API_KEY")
+SYMBOL = os.getenv("SYMBOL")
+TOPIC_NAME = os.getenv("TOPIC_NAME")
